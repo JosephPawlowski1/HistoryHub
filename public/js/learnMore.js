67a7d9ed5889aeca4d3518d2d31aa63e5f610d48
JosephPawlowski1 <joepawlowski712@gmail.com>
2019-11-19 14:33:11 -0600

    added events page and learnMore buton js

---
+++ public/js/learnMore.js
@@ -0,0 +1,19 @@
+$( "#learnMore" ).click(function() {
+    alert( "Handler for .click() called." );
+    document.location.href = 'about.html';
+  });
+
+  $( "#learnMore2" ).click(function() {
+    alert( "Handler for .click() called." );
+    document.location.href = 'about.html';
+  });
+
+  $( "#learnMore3" ).click(function() {
+    alert( "Handler for .click() called." );
+    document.location.href = 'about.html';
+  });
+
+  $( "#learnMore4" ).click(function() {
+    alert( "Handler for .click() called." );
+    document.location.href = 'about.html';
+  });
