9a76da7d296b49258cc561bf57a7f9a13a9f4b9e
Conor Kitzhoefer-Toledo <ConorKitzhoefer-toledo@Conors-MacBook-Pro-2.local>
2019-11-19 18:36:35 -0600

    Minor edits for information page

--- public/js/learnMore.js
+++ public/js/learnMore.js
@@ -1,19 +1,19 @@
 $( "#learnMore" ).click(function() {
     alert( "Handler for .click() called." );
-    document.location.href = 'event-info.html';
+    document.location.href = 'events-info.html';
   });
 
   $( "#learnMore2" ).click(function() {
     alert( "Handler for .click() called." );
-    document.location.href = 'event-info.html';
+    document.location.href = 'events-info.html';
   });
 
   $( "#learnMore3" ).click(function() {
     alert( "Handler for .click() called." );
-    document.location.href = 'event-info.html';
+    document.location.href = 'events-info.html';
   });
 
   $( "#learnMore4" ).click(function() {
     alert( "Handler for .click() called." );
-    document.location.href = 'event-info.html';
+    document.location.href = 'events-info.html';
   });
