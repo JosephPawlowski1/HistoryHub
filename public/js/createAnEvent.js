57d583b21d86a3570502b16c49997900beddb9ce
JosephPawlowski1 <joepawlowski712@gmail.com>
2019-12-02 12:45:19 -0600

    fix firebase pushing event input

--- public/js/createAnEvent.js
+++ public/js/createAnEvent.js
@@ -6,7 +6,7 @@ $("#createAnEvent").click(function () {
 
 
 
-$("#createEventBTN").click(function () {
+$("#createEventBTN").click(function (){
   // alert("btn Clicked");
   const firebaseConfig = {
     apiKey: "AIzaSyA7zxrx4StJObT7CfXMsCzKGCpbfSKnOZs",
@@ -30,29 +30,31 @@ $("#createEventBTN").click(function () {
   var location2 = $("input#location").val();
   var time2 = $("input#time").val();
   var description2 = $("textarea#description").val();
-  alert(name2);
-  alert(location2);
-  alert(time2);
-  alert(description2);
+  //alert(name2);
+  //alert(location2);
+  //alert(time2);
+  //alert(description2);
   alert("firebase refereance")
   var dbRef2 = firebase.database();
   var eventsRef2 = dbRef2.ref('events');
-  alert("after database referance")
+ alert("after database referance")
+ alert(dbRef2)
+ alert(eventsRef2)
   var data2 = {
     name: name2,
     location: location2,
     time: time2,
     description: description2
   }
-
-  alert("about to push ")
-  alert(data2.name)
-  eventsRef2.push(data2,finished(error))
-
+  alert("about to push")
+  //alert(data2.name)
+  eventsRef2.push(data2,finished);
   alert("after push")
   function finished(error) {
+    alert(error)
     if (error) {
       alert("error")
+      alert(error)
       console.log('ooops');
     } else {
       alert("data saved")
