91dc8a42651b5fdc0bb7a5d2301ae920126040e5
Conor Kitzhoefer-Toledo <conor.ktizhoefer-toledo@marquette.edu>
2020-01-27 17:25:45 -0600

    Work on learn more and class info

--- public/js/learnMore.js
+++ public/js/learnMore.js
@@ -26,9 +26,20 @@
   $(document).on("click", function(event){
     if(event.target.id == 'createAnEvent'){
       document.location.href = 'createAnEvent.html';
-    }else{
+    }else {
     document.location.href = 'events-info.html';
     event.target.id;
     sessionStorage.setItem("learnMoreNum",event.target.id);
     }
+
+});
+$(document).on("click", function(classes){
+  if(classes.target.id == 'createAclass'){
+    document.location.href = 'createAclass.html';
+  }else {
+  document.location.href = 'class-info.html';
+  classes.target.id;
+  sessionStorage.setItem("learnMoreNum",classes.target.id);
+  }
+
 });
