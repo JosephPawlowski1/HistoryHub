9fea9f60650cccd3b048ec31a0946acc8d0ebc33
Conor Kitzhoefer-Toledo <conor.ktizhoefer-toledo@marquette.edu>
2020-04-04 20:07:18 -0500

    Work on blog posts so far

---
+++ public/js/blog.js
@@ -0,0 +1,110 @@
+var eventKey;
+var classKeys;
+var blogKey;
+window.onload = function() {
+
+  var login = sessionStorage.getItem("login");
+  if(login == 0)
+  {
+    document.getElementById('createBlog').hide;
+  }
+
+  
+
+  var blogGetRef = dbRef.ref("blog");
+
+  
+  blogGetRef.on("value", gotData);
+
+
+
+  var blogObjs = [];
+  function gotData(data) {
+
+    var blogs = data.val();
+   
+    blogKey = Object.keys(blogs);
+    alert(blogKey);
+    sessionStorage.setItem("blogKey", blogKey);
+   
+    for (var i = 0; i < blogKey.length; i++) {
+
+      var key = blogKey[i];
+
+    
+      var blo = blogs[key];
+
+      var blogObj = {
+        "name": blo.name,
+        "introduction": blo.introduction,
+        "description": blo.description
+      };
+
+      blogObjs.push(blogObj);
+    }
+   
+
+
+
+    let cardContainer;
+    var counter = 0;
+    let createTaskCard = (blogObjs) => {
+
+    
+      let card = document.createElement('div');
+      card.blogName = 'col-lg-4 mb-4';
+
+      let cardHeader = document.createElement('div');
+      cardHeader.blogName = 'card h-100';
+
+      let cardHeaderInner = document.createElement('h4');
+      cardHeaderInner.blogName = 'card-header'
+      cardHeaderInner.innerHTML = blogObjs.name;
+
+
+      let cardBody = document.createElement('div');
+      cardBody.blogName = 'card-body';
+
+      let cardBodyInner = document.createElement('p');
+      cardBodyInner.blogName = 'card-text';
+      cardBodyInner.innerHTML = "Introduction: " + blogObjs.introduction;
+
+      let cardFooter = document.createElement('div');
+      cardFooter.blogName = 'card-footer';
+
+      let cardFooterInner = document.createElement('a');
+      cardFooterInner.id = counter;
+      cardFooterInner.blogName = 'btn btn-primary';
+      cardFooterInner.innerHTML = 'Learn More';
+      cardFooterInner.href = 'blog-info.html';
+
+
+
+      cardHeader.appendChild(cardHeaderInner);
+      
+      cardBody.appendChild(cardBodyInner);
+      cardFooter.appendChild(cardFooterInner);
+      cardHeader.appendChild(cardBody);
+      cardHeader.appendChild(cardFooter);
+      card.appendChild(cardHeader);
+      cardContainer.appendChild(card);
+      counter++
+
+    }
+
+    let initListOfTasks = () => {
+      if (cardContainer) {
+        document.getElementById('card-container').replaceWith(cardContainer);
+        return;
+      }
+
+      cardContainer = document.getElementById('card-container');
+      blogObjs.forEach((blogObjs) => {
+        createTaskCard(blogObjs);
+      });
+    };
+
+    initListOfTasks();
+  }
+}
+
---
+++ public/js/blog_info.js
@@ -0,0 +1,114 @@
+
+window.onload = function() {
+    var key = sessionStorage.getItem("blogKey");
+    var learnMoreNum = sessionStorage.getItem("learnMoreNum");
+    var array = key.split(",");
+    var num = Number(learnMoreNum);
+    alert(num);
+    var blogGetOneRef = dbRef.ref("blog/" + array[num]);
+  
+    blogGetOneRef.on("value", gotData);
+    function gotData(data) {
+      var blogHeader = document.getElementById("blogBodyTitle");
+      var blogBodyIntro = document.getElementById("blogBodyIntrod");
+      var blogBodyDes = document.getElementById("blogBodyDes");
+      var event = data.val();
+      blogHeader.innerHTML = event.name;
+      alert(event.name);
+      blogBodyIntro.innerHTML = event.introduction;
+      blogBodyDes.innerHTML = event.description;
+    }
+  
+    var blogCommentGetRef = dbRef.ref("blog/" + array[num] + "/comments/");
+    blogCommentGetRef.on("value", gotCommentData);
+    var blogCommentsObjs = [];
+    function gotCommentData(dataComment) {
+      var blogComments = dataComment.val();
+      console.log(blogComments);
+  
+      blogCommentsKeys = Object.keys(blogComments);
+  
+      sessionStorage.setItem("blogCommentKeys", blogCommentsKeys);
+  
+      for (var i = 0; i < blogCommentsKeys.length; i++) {
+        console.log(blogCommentsKeys[i]);
+        var key = blogCommentsKeys[i];
+        var blogComment = blogComments[key].comment;
+        console.log(blogComment.email);
+        console.log(blogComment.text);
+        console.log(blogComment.postTime);
+  
+        var blogCommentObj = {
+          email: blogComment.email,
+          text: blogComment.text,
+          postTime: blogComment.postTime
+        }
+        //    console.log(blogCommentObj);
+  
+        blogCommentsObjs.push(blogCommentObj);
+        console.log(blogCommentsObjs);
+      }
+  
+  
+    
+        let commentContainer;
+        var counter = 0;
+  
+        let createTaskCard = blogCommentsObjs => {
+          let card = document.createElement("div");
+          card.blogName = "media mb-4";
+          card.id = counter;
+  
+          let cardImg = document.createElement("img");
+          cardImg.blogName = "d-flex mr-3 rounded-circle";
+          cardImg.src = "imgs/funny.gif";
+          cardImg.width = "100";
+          cardImg.height = "100";
+          cardImg.alt = "";
+  
+          let cardBasket = document.createElement("div");
+          cardBasket.blog = "media-body";
+  
+          let cardHeader = document.createElement("h5");
+          cardHeader.blogName = "mt-0";
+          cardHeader.innerHTML = blogCommentsObjs.email;
+  
+          let cardBody = document.createElement("a");
+          cardBody.innerHTML = blogCommentsObjs.text;
+  
+          cardBasket.appendChild(cardHeader);
+          cardBasket.appendChild(cardBody);
+          card.appendChild(cardImg);
+          card.appendChild(cardBasket);
+          console.log(card);
+          console.log(commentContainer);
+          commentContainer.appendChild(card);
+          counter++;
+        };
+        let initListOfTasks = () => {
+          if (commentContainer) {
+            document.getElementById("card-container").replaceWith(card);
+            return;
+          }
+          commentContainer = document.getElementById("card-container");
+          blogCommentsObjs.forEach(blogCommentObjs => {
+            createTaskCard(blogCommentObjs);
+          });
+        };
+  
+        initListOfTasks();
+      }
+    }
+  
+    $("#deleteBlogBTN").on("click", function(event) {
+  
+      blogKeys = sessionStorage.getItem("blogKeys");
+      var learnMoreNum = sessionStorage.getItem("learnMoreNum");
+      var array = blogKeys.split(",");
+      var num = Number(learnMoreNum);
+      var blogGetOneRefDelete = dbRef.ref("blog/" + array[num]);
+      blogGetOneRefDelete.remove();
+      document.location.href = "blog-home-1.html";
+    });
+  
+  
