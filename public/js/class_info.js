c15cad93c85037551407d9f8c6db8769576d74ab
Sonny Darji <SonnyD@Sonnys-MacBook-Air-2.local>
2020-04-18 18:08:31 -0500

    removed alert from classpage

--- public/js/class_info.js
+++ public/js/class_info.js
@@ -1,4 +1,3 @@
-
 window.onload = function() {
   var key = sessionStorage.getItem("classKeys");
   var learnMoreNum = sessionStorage.getItem("learnMoreNum");
@@ -29,7 +28,7 @@ window.onload = function() {
     //console.log(classComments);
 
     classCommentsKeys = Object.keys(classComments);
-    alert(classCommentsKeys);
+    //alert(classCommentsKeys);
     sessionStorage.setItem("classCommentKeys", classCommentsKeys);
 
     for (var i = 0; i < classCommentsKeys.length; i++) {
@@ -43,73 +42,69 @@ window.onload = function() {
       var classCommentObj = {
         email: classComment.email,
         text: classComment.text,
-        postTime: classComment.postTime
-      }
+        postTime: classComment.postTime,
+      };
       //    console.log(classCommentObj);
 
       classCommentsObjs.push(classCommentObj);
       //console.log(classCommentsObjs);
     }
 
+    let commentContainer;
+    var counter = 0;
 
-  
-      let commentContainer;
-      var counter = 0;
+    let createTaskCard = (classCommentsObjs) => {
+      let card = document.createElement("div");
+      card.className = "media mb-4";
+      card.id = counter;
 
-      let createTaskCard = classCommentsObjs => {
-        let card = document.createElement("div");
-        card.className = "media mb-4";
-        card.id = counter;
+      let cardImg = document.createElement("img");
+      cardImg.className = "d-flex mr-3 rounded-circle";
+      cardImg.src = "imgs/funny.gif";
+      cardImg.width = "100";
+      cardImg.height = "100";
+      cardImg.alt = "";
 
-        let cardImg = document.createElement("img");
-        cardImg.className = "d-flex mr-3 rounded-circle";
-        cardImg.src = "imgs/funny.gif";
-        cardImg.width = "100";
-        cardImg.height = "100";
-        cardImg.alt = "";
+      let cardBasket = document.createElement("div");
+      cardBasket.class = "media-body";
 
-        let cardBasket = document.createElement("div");
-        cardBasket.class = "media-body";
+      let cardHeader = document.createElement("h5");
+      cardHeader.className = "mt-0";
+      cardHeader.innerHTML = classCommentsObjs.email;
 
-        let cardHeader = document.createElement("h5");
-        cardHeader.className = "mt-0";
-        cardHeader.innerHTML = classCommentsObjs.email;
+      let cardBody = document.createElement("a");
+      cardBody.innerHTML = classCommentsObjs.text;
 
-        let cardBody = document.createElement("a");
-        cardBody.innerHTML = classCommentsObjs.text;
+      cardBasket.appendChild(cardHeader);
+      cardBasket.appendChild(cardBody);
+      card.appendChild(cardImg);
+      card.appendChild(cardBasket);
+      //console.log(card);
+      // console.log(commentContainer);
+      commentContainer.appendChild(card);
+      counter++;
+    };
+    let initListOfTasks = () => {
+      if (commentContainer) {
+        document.getElementById("card-container").replaceWith(card);
+        return;
+      }
+      commentContainer = document.getElementById("card-container");
+      classCommentsObjs.forEach((classCommentObjs) => {
+        createTaskCard(classCommentObjs);
+      });
+    };
 
-        cardBasket.appendChild(cardHeader);
-        cardBasket.appendChild(cardBody);
-        card.appendChild(cardImg);
-        card.appendChild(cardBasket);
-        //console.log(card);
-       // console.log(commentContainer);
-        commentContainer.appendChild(card);
-        counter++;
-      };
-      let initListOfTasks = () => {
-        if (commentContainer) {
-          document.getElementById("card-container").replaceWith(card);
-          return;
-        }
-        commentContainer = document.getElementById("card-container");
-        classCommentsObjs.forEach(classCommentObjs => {
-          createTaskCard(classCommentObjs);
-        });
-      };
-
-      initListOfTasks();
-    }
+    initListOfTasks();
   }
+};
 
-  $("#deleteClassBTN").on("click", function(event) {
-
-    classKeys = sessionStorage.getItem("classKeys");
-    var learnMoreNum = sessionStorage.getItem("learnMoreNum");
-    var array = classKeys.split(",");
-    var num = Number(learnMoreNum);
-    var classGetOneRefDelete = dbRef.ref("class/" + array[num]);
-    classGetOneRefDelete.remove();
-    document.location.href = "class.html";
-  });
-
+$("#deleteClassBTN").on("click", function(event) {
+  classKeys = sessionStorage.getItem("classKeys");
+  var learnMoreNum = sessionStorage.getItem("learnMoreNum");
+  var array = classKeys.split(",");
+  var num = Number(learnMoreNum);
+  var classGetOneRefDelete = dbRef.ref("class/" + array[num]);
+  classGetOneRefDelete.remove();
+  document.location.href = "class.html";
+});
